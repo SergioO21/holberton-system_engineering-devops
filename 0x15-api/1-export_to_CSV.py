ee0ba3430b5cac97487ed8b0606ab9fcb15ef1cb
Sergio Orejarena <sergioorueda@gmail.com>
2021-09-13 20:21:09 -0500

    Try

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,18 +1,25 @@
 #!/usr/bin/python3
-"""script to export data in the CSV format"""
-
+"""Export to CSV"""
+import csv
+import requests
+from sys import argv
 
 if __name__ == "__main__":
-    import requests
-    from sys import argv
-    import csv
-    url = "https://jsonplaceholder.typicode.com/"
     user_id = argv[1]
-    user = requests.get(url + "users/{}".format(user_id)).json()
-    tasks = requests.get(url + "todos/?userId={}".format(user_id)).json()
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/" + user_id)
+    todo_keys = {"userId": user_id}
+    tasks = requests.get("{}todos".format(url), params=todo_keys)
+    employee_name = user.json().get("username")
 
-    with open("{}.csv".format(user_id), "w") as csvfile:
-        writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
-        for task in tasks:
-            writer.writerow([user_id, user["username"],
-                            task["completed"], task["title"]])
+    with open('{}.csv'.format(user_id), mode='w') as csv_file:
+        writer = csv.writer(csv_file,
+                            delimiter=',',
+                            quotechar='"',
+                            quoting=csv.QUOTE_ALL)
+
+        for task in tasks.json():
+            writer.writerow([user_id,
+                             employee_name,
+                             task.get('completed'),
+                             task.get('title')])
