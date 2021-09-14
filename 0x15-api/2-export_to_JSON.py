2438d4db185567c960fc07ebbe1f5a60ebe4eb32
Sergio Orejarena <sergioorueda@gmail.com>
2021-09-13 21:07:24 -0500

    Export to json

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,24 +1,42 @@
 #!/usr/bin/python3
-"""Export to JSON """
+""" Export data in the JSON format. """
+
 import json
 import requests
 from sys import argv
 
+
+def main():
+    """ Returns information about his/her TODO list progress """
+    url = "https://jsonplaceholder.typicode.com"
+
+    TOTAL_NUMBER_OF_TASKS = requests.get(
+        "{}/todos?userId={}".format(url, argv[1])).json()
+    NUMBER_OF_DONE_TASKS = len(requests.get(
+        "{}/todos?userId={}&completed=true".format(url, argv[1])).json())
+    EMPLOYEE_NAME = requests.get(
+        "{}/users/{}".format(url, argv[1])).json()["name"]
+    USERNAME = requests.get(
+        "{}/users/{}".format(url, argv[1])).json()["username"]
+
+    print("Employee {} is done with tasks({}/{}):".format(
+        EMPLOYEE_NAME, NUMBER_OF_DONE_TASKS, len(TOTAL_NUMBER_OF_TASKS)))
+
+    task_list = []
+
+    for task in TOTAL_NUMBER_OF_TASKS:
+        if task["completed"]:
+            print("\t {}".format(task["title"]))
+
+        task_list.append({"task": task["title"],
+                          "completed": task["completed"],
+                          "username": USERNAME})
+
+    task_list = {argv[1]: task_list}
+
+    with open("USER_ID.json", "w") as file:
+        json.dump(task_list, file)
+
+
 if __name__ == "__main__":
-    user_id = argv[1]
-    url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(url + "users/" + user_id)
-    todo_keys = {"userId": user_id}
-    tasks = requests.get("{}todos".format(url), params=todo_keys)
-    employee_name = user.json().get("username")
-    l_dict = []
-
-    for task in tasks.json():
-        l_dict.append(dict(task=task.get("title"),
-                           completed=task.get("completed"),
-                           username=employee_name))
-
-    data = {argv[1]: l_dict}
-
-    with open('{}.json'.format(user_id), 'w') as file:
-        json.dump(data, file)
+    main()
