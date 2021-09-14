5dc9518ad025262be280c0cfc8571950417c06ef
Sergio Orejarena <sergioorueda@gmail.com>
2021-09-13 21:24:14 -0500

    Export to CSV

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,25 +1,42 @@
 #!/usr/bin/python3
-"""Export to CSV"""
+""" Export data in the JSON format """
+
 import csv
 import requests
 from sys import argv
 
+
+def main():
+    """ Returns information about his/her TODO list progress """
+    url = "https://jsonplaceholder.typicode.com"
+
+    TOTAL_NUMBER_OF_TASKS = requests.get(
+        "{}/todos?userId={}".format(url, argv[1])).json()
+    NUMBER_OF_DONE_TASKS = len(requests.get(
+        "{}/todos?userId={}&completed=true".format(url, argv[1])).json())
+    EMPLOYEE_NAME = requests.get(
+        "{}/users/{}".format(url, argv[1])).json()["name"]
+    USERNAME = requests.get(
+        "{}/users/{}".format(url, argv[1])).json()["username"]
+
+    print("Employee {} is done with tasks({}/{}):".format(
+        EMPLOYEE_NAME, NUMBER_OF_DONE_TASKS, len(TOTAL_NUMBER_OF_TASKS)))
+
+    task_list = []
+
+    for task in TOTAL_NUMBER_OF_TASKS:
+        if task["completed"]:
+            print("\t {}".format(task["title"]))
+
+        task_list.append([argv[1], USERNAME, task["completed"], task["title"]])
+
+    with open("USER_ID.csv", "w") as file:
+        writer = csv.writer(file, delimiter=',',
+                            quotechar='"', quoting=csv.QUOTE_ALL)
+
+        for row in task_list:
+            writer.writerow(row)
+
+
 if __name__ == "__main__":
-    user_id = argv[1]
-    url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(url + "users/" + user_id)
-    todo_keys = {"userId": user_id}
-    tasks = requests.get("{}todos".format(url), params=todo_keys)
-    employee_name = user.json().get("username")
-
-    with open('{}.csv'.format(user_id), mode='w') as csv_file:
-        writer = csv.writer(csv_file,
-                            delimiter=',',
-                            quotechar='"',
-                            quoting=csv.QUOTE_ALL)
-
-        for task in tasks.json():
-            writer.writerow([user_id,
-                             employee_name,
-                             task.get('completed'),
-                             task.get('title')])
+    main()
